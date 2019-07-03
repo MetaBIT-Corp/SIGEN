5f68f014f832665bd312a34c1cd2a69a3a1ac225
Enrique Menjívar <mt16007@ues.edu.sv>
2019-07-03 09:42:41 -0600

    Creacion de roles admin, docente y estudiante

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    
+    public function run(){
+
+    	User::create([
+    		'name' => 'Admin',
+    		'email' => 'admin@gmail.com',
+    		'password' => bcrypt('sigen2019'),
+    		'role' => 0
+    	]);
+
+    	User::create([
+    		'name' => 'Docente',
+    		'email' => 'docente@gmail.com',
+    		'password' => bcrypt('sigen2019'),
+    		'role' => 1
+    	]);
+
+    	User::create([
+    		'name' => 'Estudiante',
+    		'email' => 'estudiante@gmail.com',
+    		'password' => bcrypt('sigen2019'),
+    		'role' => 2
+    	]);
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,21 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script
+        src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous">
+    </script>
+    <script 
+        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" 
+        integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" 
+        crossorigin="anonymous">
+    </script>
+    <script 
+        src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" 
+        integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" 
+        crossorigin="anonymous">
+    </script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -18,6 +33,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://bootswatch.com/4/materia/bootstrap.css">
 </head>
 <body>
     <div id="app">
