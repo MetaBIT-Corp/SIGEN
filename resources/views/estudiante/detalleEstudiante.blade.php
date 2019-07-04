59a6978892d17ddb4e8f50e517bff19301625696
Pablo <diazcolato1997@gmail.com>
2019-07-04 15:11:55 -0600

    Cambio

--- resources/views/estudiante/detalleEstudiante.blade.php
+++ resources/views/estudiante/detalleEstudiante.blade.php
@@ -3,18 +3,18 @@
 
 @section("ol_breadcrumb")
     <li class="breadcrumb-item"><a href="#">Materia</a></li>
-    <li class="breadcrumb-item"><a href="#">Estudiante</a></li>
-    <li class="breadcrumb-item">Detalle</li>
+    <li class="breadcrumb-item"><a href="#">Listado de estudiantes</a></li>
+    <li class="breadcrumb-item">Detalle de estudiante</li>
 @endsection
 
 @section("main")
     
-    <h2 style="padding-bottom:50px;">Detalle de Estudiante</h2>
-
     @if($estudiante)
+        <h2 style="padding-bottom:30px;">Detalle de Estudiante</h2>
+       
         <table class="table table-bordered" style="width:600px; margin: 0 auto;">
             <tr>
-                <th scope="row">Carnet</th>
+                <th scope="row" style="width:200px;">Carnet</th>
                 <td>{{$estudiante->carnet}}</td>
             </tr>
             <tr>
