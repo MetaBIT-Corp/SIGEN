a30bb4509903c87473c34bbac1aae5ebd0485101
Edwin Palacios <ap16014@ues.edu.sv>
2019-07-04 22:58:49 -0600

    correcion materia-ciclo

--- app/Http/Controllers/ListadoEstudianteController.php
+++ app/Http/Controllers/ListadoEstudianteController.php
@@ -8,12 +8,14 @@ use DB;
 class ListadoEstudianteController extends Controller
 {
     //
-    public function listar($carga_academica){
+    public function listar($id_mat_ci){
     	$estudiantes=DB::table('estudiante')
     	->join('detalle_insc_est','estudiante.id_est','=','detalle_insc_est.id_est')
     	->join('carga_academica','carga_academica.id_carg_aca','=','detalle_insc_est.id_carg_aca')
-    	->where('carga_academica.id_carg_aca','=',$carga_academica)
+    	->join('materia_ciclo','materia_ciclo.id_mat_ci','=','carga_academica.id_mat_ci')
+    	->where('materia_ciclo.id_mat_ci','=',$id_mat_ci)
     	->select('estudiante.*')->get();
+    	
     	return view("estudiante/listadoEstudiante",compact("estudiantes"));
     }
 }
