7d4a5cd42537056c0cd3d320c1be5c3f81c915ba
Pablo <diazcolato1997@gmail.com>
2019-07-15 19:45:49 -0600

    Agregando auth en constructor

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -17,6 +17,11 @@ class EstudianteController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+     public function __construct()
+     {
+	$this->middleware('auth');
+     }
+
     public function index()
     {
         //
