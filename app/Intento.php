7bfd03ba373892ba4ab07b4f7bb8fcc43f9729fa
Enrique Menjívar <mt16007@ues.edu.sv>
2019-09-30 14:59:52 -0600

    Soluciones de bug en modalidad respuesta corta

--- app/Intento.php
+++ app/Intento.php
@@ -41,42 +41,66 @@ class Intento extends Model
         foreach($intento->respuestas as $respuesta){
 
             //Si la respuesta que seleccionó en la pregunta es correcta
-            if($respuesta->opcion->correcta==1){
+            if($respuesta->opcion != null){
+                if($respuesta->opcion->correcta==1){
 
-                //Obtener la pregunta a la que pertenece la respuesta
-                $pregunta_id = $respuesta->pregunta->id;
+                    //Obtener la pregunta a la que pertenece la respuesta
+                    $pregunta_id = $respuesta->pregunta->id;
 
-                //Consulta para obtener el objeto clave_area_pregunta_estudiante al que pertenece la pregunta
-                $cape = Clave_Area_Pregunta_Estudiante::where('estudiante_id', $estudiante_id)
-                                                        ->where('pregunta_id', $pregunta_id)
-                                                        ->first();
+                    //Consulta para obtener el objeto clave_area_pregunta_estudiante al que pertenece la pregunta
+                    $cape = Clave_Area_Pregunta_Estudiante::where('estudiante_id', $estudiante_id)
+                                                            ->where('pregunta_id', $pregunta_id)
+                                                            ->first();
+            
+                    //Obtener la clave_aera a la que pertenece la pregunta
+                    $clave_area = $cape->clave_area;
+
+                    //Obtener la modalidad a la que pertecene la pregunta
+                    $modalidad = $clave_area->area->tipo_item_id;
+
+                    //Obtener el peso de la pregunta
+                    $peso = $clave_area->peso;
+
+                    //Cuenta la cantidad de preguntas que tiene el objeto clave_are
+                    $cantidad_preguntas = count($clave_area->claves_areas_preguntas_estudiante);
         
-                //Obtener la clave_aera a la que pertenece la pregunta
-                $clave_area = $cape->clave_area;
-
-                //Obtener la modalidad a la que pertecene la pregunta
-                $modalidad = $clave_area->area->tipo_item_id;
-
-                //Obtener el peso de la pregunta
-                $peso = $clave_area->peso;
-
-                //Cuenta la cantidad de preguntas que tiene el objeto clave_are
-                $cantidad_preguntas = count($clave_area->claves_areas_preguntas_estudiante);
-
-                //Verifica si la pregunta pertenece a modalidad de respuesta corta
-                if($modalidad==4){
-                    $txt_respuesta = strtolower($respuesta->texto_respuesta);
-                    $txt_opcion = strtolower($respuesta->opcion->opcion);
-                    if(strcmp($txt_respuesta, $txt_opcion) == 0){
-                        
-                        //Calcula la ponderación de la pregunta
-                        $nota += ($peso/$cantidad_preguntas)/10;
-                    }
-                }else{
-                    //Calcula la ponderación de la pregunta
+                   //Calcula la ponderación de la pregunta
                     $nota += ($peso/$cantidad_preguntas)/10;
                 }
+            }else{
+                    //Obtener la pregunta a la que pertenece la respuesta
+                    $pregunta_id = $respuesta->pregunta->id;
+
+                    //Consulta para obtener el objeto clave_area_pregunta_estudiante al que pertenece la pregunta
+                    $cape = Clave_Area_Pregunta_Estudiante::where('estudiante_id', $estudiante_id)
+                                                            ->where('pregunta_id', $pregunta_id)
+                                                            ->first();
+            
+                    //Obtener la clave_aera a la que pertenece la pregunta
+                    $clave_area = $cape->clave_area;
+
+                    //Obtener la modalidad a la que pertecene la pregunta
+                    $modalidad = $clave_area->area->tipo_item_id;
+
+                    //Obtener el peso de la pregunta
+                    $peso = $clave_area->peso;
+
+                    //Cuenta la cantidad de preguntas que tiene el objeto clave_are
+                    $cantidad_preguntas = count($clave_area->claves_areas_preguntas_estudiante);
+
+                    //Verifica si la pregunta pertenece a modalidad de respuesta corta
+                    if($modalidad==4){
+                        $txt_respuesta = strtolower($respuesta->texto_respuesta);
+                        $txt_opcion = strtolower($respuesta->pregunta->opcion->opcion);
+                        if(strcmp($txt_respuesta, $txt_opcion) == 0){
+                            
+                            //Calcula la ponderación de la pregunta
+                            $nota += ($peso/$cantidad_preguntas)/10;
+                        }
+                    }
+
             }
+            
         }
 
         return $nota;
