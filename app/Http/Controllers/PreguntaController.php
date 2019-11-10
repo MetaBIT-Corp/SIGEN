7ea4e757c566c1fd743c4998d9b2fec6fb18ff9a
Ricardo Estupinian <ricaeslo@gmail.com>
2019-11-10 11:30:30 -0600

    H21 Importacion de modalidad de respuesta corta, totalmente funcional

--- app/Http/Controllers/PreguntaController.php
+++ app/Http/Controllers/PreguntaController.php
@@ -248,69 +248,115 @@ class PreguntaController extends Controller
         $message=['error'=>'Hubo un error en la importacion. Verifique que sea el formato adecuado.','type'=>1];
         //$message=['success'=>'La importacion de preguntas se efectuo exitosamente.','type'=>2];
         //Se hara la importacion de las preguntas segun la modalidad del area
-         switch ($modalidad_area) {
+
+        $spreadsheet=null;
+        $data=null;
+        try{
+            //Se carga el archivo que subio el archivo para poder acceder a los datos
+            $spreadsheet = IOFactory::load(storage_path($path = "app\\".str_replace("/","\\",$ruta)));
+
+            //Todas las filas se convierten en un array que puede ser accedido por las letras de las columnas de archivo excel
+            $data = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+        }catch(Exception $e){
+            return response()->json($message);
+        }
+
+        //Condicional segun modalidad del area, se procedera de manera diferente en la importacion
+        switch ($modalidad_area) {
             case 1:
                 
                 break;
             case 2:
-                try{
-                    //Se carga el archivo que subio el archivo para poder acceder a los datos
-                    $spreadsheet = IOFactory::load(storage_path($path = "app\\".str_replace("/","\\",$ruta)));
+                //Se verifica el codigo de plantilla
+                if($spreadsheet->getActiveSheet()->getCell('F6')=="2FV"){
+                    for($i=2;$i<=count($data);$i++){
+                    //Se verifica que la columna en la que esta la pregunta no este vacia
+                        if($data[$i]["A"]!=""){
+                            //Se crea el grupo de emparejamiento
+                            $gpo=new Grupo_Emparejamiento();
+                            $gpo->area_id=$area->id;
+                            $gpo->save();
 
-                    if($spreadsheet->getActiveSheet()->getCell('A1')=="Pregunta"&&$spreadsheet->getActiveSheet()->getCell('B1')=="OpcionCorrecta"){
-                        //Todas las filas se convierten en un array que puede ser accedido por las letras de las columnas de archivo excel
-                        $data = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
-                        for($i=2;$i<=count($data);$i++){
-                        //Se verifica que la columna en la que esta la pregunta no este vacia
-                            if($data[$i]["A"]!=""){
-                                //Se crea el grupo de emparejamiento
-                                $gpo=new Grupo_Emparejamiento();
-                                $gpo->area_id=$area->id;
-                                $gpo->save();
+                            //Se crea la pregunta segun la columna A
+                            $preg=new Pregunta();
+                            $preg->grupo_emparejamiento_id=$gpo->id;
+                            $preg->pregunta=$data[$i]["A"];
+                            $preg->save();
 
-                                //Se crea la pregunta segun la columna A
-                                $preg=new Pregunta();
-                                $preg->grupo_emparejamiento_id=$gpo->id;
-                                $preg->pregunta=$data[$i]["A"];
-                                $preg->save();
+                            //Creacion de opciones falso/verdaero
+                            $opv=new Opcion();
+                            $opv->pregunta_id=$preg->id;
+                            $opv->opcion="Verdadero";
 
-                                //Creacion de opciones falso/verdaero
-                                $opv=new Opcion();
-                                $opv->pregunta_id=$preg->id;
-                                $opv->opcion="Verdadero";
+                            $opf=new Opcion();
+                            $opf->pregunta_id=$preg->id;
+                            $opf->opcion="Falso";
 
-                                $opf=new Opcion();
-                                $opf->pregunta_id=$preg->id;
-                                $opf->opcion="Falso";
+                            //Si hay cualquier otra letra,esta vacia o v la correcta sera Verdadero por defecto
+                            $opf->correcta=0;
+                            $opv->correcta=1;
 
-                                //Si hay cualquier otra letra,esta vacia o v la correcta sera Verdadero por defecto
-                                $opf->correcta=0;
-                                $opv->correcta=1;
-
-                                //Caso si es falsa
-                                if(strtolower($data[$i]["B"])=="f"){
-                                   $opf->correcta=1;
-                                   $opv->correcta=0;
-                                }
-                                $opf->save();
-                                $opv->save();
+                            //Caso si es falsa
+                            if(strtolower($data[$i]["B"])=="f"){
+                               $opf->correcta=1;
+                               $opv->correcta=0;
                             }
+                            $opv->save();
+                            $opf->save();
                         }
-                        $message=['success'=>'La importacion de preguntas se efectuo exitosamente.','type'=>2];
                     }
-
-                }catch(ErrorException $e){
-                    $message=['error'=>'Hubo un error en la importacion. Verifique que sea el formato adecuado.','type'=>1];
-                }
+                    $message=['success'=>'La importacion de preguntas se efectuo exitosamente.','type'=>2];
+                }                
                 break;
             case 3:
 
                 break;
             case 4:
+                //Se carga el archivo que subio el archivo para poder acceder a los datos
+                $spreadsheet = IOFactory::load(storage_path($path = "app\\".str_replace("/","\\",$ruta)));
 
+                //Se verifica el codigo de plantilla
+                if($spreadsheet->getActiveSheet()->getCell('B4')=="4TC"){
+                    //dd($data);
+                    //Recuperando las columnas para acceder por columnas
+                    $columns=null;
+                    foreach ($data[1] as $key => $value) {
+                        $columns[]=$key;
+                    }
+
+                    //dd($columns);
+                    for ($j=1; $j < count($columns)-1; $j++){
+                        if($data[6][$columns[$j]]!=null){
+                            //Se crea el grupo de emparejamiento
+                            $gpo=new Grupo_Emparejamiento();
+                            $gpo->area_id=$area->id;
+                            $gpo->save();
+
+                            //Se crea la pregunta segun la columna A
+                            $preg=new Pregunta();
+                            $preg->grupo_emparejamiento_id=$gpo->id;
+                            $preg->pregunta=$data[6][$columns[$j]];
+                            $preg->save();
+                       
+                            for ($i=7; $i <= count($data) ; $i++) { 
+                                if($data[$i][$columns[$j]]!=null){
+                                    //Creacion de opciones falso/verdaero
+                                    $op=new Opcion();
+                                    $op->pregunta_id=$preg->id;
+                                    $op->opcion=$data[$i][$columns[$j]];
+                                    $op->correcta=1;
+                                    $op->save();
+                                }
+                            }
+                        }
+                    }
+                    $message=['success'=>'La importacion de preguntas se efectuo exitosamente.','type'=>2];
+                }
+                
                 break;
         }
 
+        //Eliminar el archivo subido, solo se utiliza para la importacion y luego de desecha
         return response()->json($message);
     }
 }
